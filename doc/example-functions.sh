2be5ae6762aa917bc4702df7ca063f0563b31726
nowrep <nowrep@gmail.com>
2013-12-28 17:41:12 +0100

    Doc: NOTIFY_ID var should be exported

--- doc/example-functions.sh
+++ doc/example-functions.sh
@@ -8,34 +8,35 @@
 function notify {
     argument=""
     last_id=`echo $NOTIFY_ID | sed 's/[^0-9]//g'`
-        
+
     if [ -n "$last_id" ]; then
         argument="-r $last_id"
-        NOTIFY_ID=$last_id        
+        NOTIFY_ID=$last_id
     fi
-    
+
     NOTIFY_ID=`notify-desktop $argument "$@"`
+    export NOTIFY_ID
 }
 
 #
 # Function that replaces notifications sent from one script
 # accross multiple runs of this script
-# 
+#
 # - it saves notification id in temporary file
 
 function notify_ {
     this=`basename $0`
     tmp_file=/tmp/${this//./-}.txt
     argument=""
-    
+
     if [ -e $tmp_file ]; then
         last_id=`cat $tmp_file | sed 's/[^0-9]//g'`
-        
+
         if [ -n "$last_id" ]; then
             argument="-r $last_id"
         fi
     fi
-    
+
     notify-desktop $argument "$@" > $tmp_file
 }
 
@@ -45,4 +46,4 @@ function notify_ {
 #
 # notify -i up "Show icon" "Up icon is shown in notification"
 # notify_ "Testing notification"
-# 
+#
